63b32e4a162a3b166fb8205855cdedcef225f6fd
jamaldemir <jamal@cybertekschool.com>
2021-09-04 15:41:26 -0400

    ConfigReader.java added

---
+++ src/test/java/utilities/ConfigReader.java
@@ -0,0 +1,24 @@
+package utilities;
+
+
+import net.serenitybdd.core.environment.EnvironmentSpecificConfiguration;
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.SystemEnvironmentVariables;
+
+public class ConfigReader {
+
+    private static EnvironmentVariables environmentVariables ;
+    static {
+        environmentVariables =
+                SystemEnvironmentVariables.createEnvironmentVariables();
+    }
+
+    public static String getProperty(String propertyName){
+
+        return EnvironmentSpecificConfiguration
+                .from(environmentVariables)
+                .getProperty(propertyName);
+
+    }
+
+}
