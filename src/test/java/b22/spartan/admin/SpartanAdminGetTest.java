9184632c46a46d67eef82b03825a9922b9391016
jamaldemir <jamal@cybertekschool.com>
2021-09-04 11:54:17 -0400

    SerenityRest imports added

--- src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -2,14 +2,17 @@ package b22.spartan.admin;
 
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.SerenityRest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import static net.serenitybdd.rest.SerenityRest.given;
 
 @SerenityTest
 public class SpartanAdminGetTest {
@@ -31,6 +34,22 @@ public class SpartanAdminGetTest {
         .when()
                 .get("/api/spartans")
         .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void getOneSpartan(){
+
+            given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+                .pathParam("id",15)
+                .when()
+                .get("/api/spartans/{id}")
+                .then()
                 .statusCode(200)
                 .and()
                 .contentType(ContentType.JSON);
