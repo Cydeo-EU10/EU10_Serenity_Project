77af389857320bc655be24806ef30144b6ac5151
jamaldemir <jamal@cybertekschool.com>
2021-09-04 10:54:35 -0400

    first

--- src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -1,4 +1,37 @@
 package b22.spartan.admin;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class SpartanAdminGetTest {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://44.195.19.167:7000";
+
+    }
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+        .when()
+                .get("/api/spartans")
+        .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+
+    }
+
 }
