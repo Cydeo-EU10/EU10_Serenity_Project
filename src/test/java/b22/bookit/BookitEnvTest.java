6a6aae1ef12717d41bef98f29d15f95ed4c592c2
jamaldemir <jamal@cybertekschool.com>
2021-09-04 16:21:03 -0400

    serenity.conf added

--- src/test/java/b22/bookit/BookitEnvTest.java
+++ src/test/java/b22/bookit/BookitEnvTest.java
@@ -1,15 +1,19 @@
 package b22.bookit;
 
+import net.serenitybdd.junit5.SerenityTest;
 import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
 
+@SerenityTest
 public class BookitEnvTest {
 
 
         @Test
          public void test1(){
 
-
-
+            System.out.println(ConfigReader.getProperty("base.url"));
+            System.out.println(ConfigReader.getProperty("teacher.username"));
+            System.out.println(ConfigReader.getProperty("teacher.password"));
 
         }
 
