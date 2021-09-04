60373a5ad5c400efbf23d334dbe965e26f1d13ab
jamaldemir <jamal@cybertekschool.com>
2021-09-04 15:39:08 -0400

    SpartanData.csv added

--- src/test/java/b22/spartan/editor/SpartanEditorPostTest.java
+++ src/test/java/b22/spartan/editor/SpartanEditorPostTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utilities.SpartanUtil;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.baseURI;
@@ -90,8 +91,14 @@ public class SpartanEditorPostTest extends SpartanNewBase {
                     );
 
         }
+        /*
+            we can give name to each execution using name = ""
+            and if you want to get index of iteration we can use {index}
+            and also if you to include parameter in your test name
+            {0} , {1},{2} --> based on the order you provide as argument.
 
-        @ParameterizedTest
+         */
+        @ParameterizedTest(name = "New Spartan {index} - name: {0}")
         @CsvFileSource(resources = "/SpartanData.csv",numLinesToSkip = 1)
         public void postSpartanWithCSV(String name,String gender,long phone){
 
@@ -99,6 +106,55 @@ public class SpartanEditorPostTest extends SpartanNewBase {
             System.out.println("gender = " + gender);
             System.out.println("phone = " + phone);
 
+            Map<String,Object> bodyMap = new LinkedHashMap<>();
+            bodyMap.put("name",name);
+            bodyMap.put("gender",gender);
+            bodyMap.put("phone",phone);
+
+            System.out.println("bodyMap = " + bodyMap);
+
+            //send a post request as editor
+            given()
+                    .auth().basic("editor","editor")
+                    .accept(ContentType.JSON)
+                    .contentType(ContentType.JSON)
+                    .body(bodyMap)
+                    .log().body()
+                    .when()
+                    .post("/spartans")
+                    .then().log().all();
+
+            //status code is 201
+            Ensure.that("Status code is 201", x -> x.statusCode(201));
+            //content type is Json
+            Ensure.that("Content type is JSON", vR -> vR.contentType(ContentType.JSON));
+            //success message is A Spartan is Born!
+            Ensure.that("success message is correct",
+                    thenPart -> thenPart.body("success",is("A Spartan is Born!"))
+            );
+            //id is not null
+            Ensure.that("id is not null",
+                    thenPart -> thenPart.body("data.id",notNullValue())
+            );
+            //name is correct
+            Ensure.that("name is correct",
+                    thenPart -> thenPart.body("data.name",is(bodyMap.get("name")))
+            );
+            //gender is correct
+            Ensure.that("gender is correct",
+                    thenPart -> thenPart.body("data.gender",is(bodyMap.get("gender")))
+            );
+            //phone is correct
+            Ensure.that("phone is correct",
+                    thenPart -> thenPart.body("data.phone",is(bodyMap.get("phone")))
+            );
+            //check location header ends with newly generated id
+            //get id and save
+            String id = lastResponse().jsonPath().getString("data.id");
+
+            Ensure.that("check location header ends with newly generated id",
+                    vR -> vR.header("Location",endsWith(id))
+            );
 
         }
 
